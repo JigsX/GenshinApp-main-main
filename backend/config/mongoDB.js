6c4c03940c8e9033367cc0862b224c589d465b6d
Jigger Velasco <101715828+JigsX@users.noreply.github.com>
2025-02-24 13:37:04 +0800

    Update mongoDB.js

--- backend/config/mongoDB.js
+++ backend/config/mongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDatabase = async () => {
     try{
-        await mongoose.connect("mongodb://localhost:27017/Genshin");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log(`Database Status: ${mongoose.connection.host}`)
     } catch(error){
         console.error(error.message);
